3d9248499752c1570f59102fdd39acbd67f0a458
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-06-16 20:28:17 -0300

    init

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,70 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public CatalogController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);    
+        }
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductById(string id)
+        {
+            var products = await _productRepository.GetProduct(id);
+            if(products is null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            if (category is null)
+            {
+                return BadRequest("Invalid category");
+            }
+            var products = await _productRepository.GetProductByCategory(category);
+            
+            return Ok(products);
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<Product>>> UpdateProduct([FromBody] Product product)
+        {
+            if (product is null)
+            {
+                return BadRequest("Invalid product");
+            }
+            var products = await _productRepository.UpdateProduct(product);
+
+            return Ok(products);
+        }
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        public async Task<ActionResult> DeleteProductById(string id)
+        {
+            return Ok(await _productRepository.DeleteProduct(id));
+        }
+    }
+}
---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,50 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "4357953845846387f76e8r6347428",
+                    Name = "Texto teste 1",
+                    Description = "Description",
+                    Image = "product-1.png",
+                    Price = 9500,
+                    Category = "Category 1"
+                },
+                new Product()
+                {
+                    Id = "4357953845834543587f76e8r6347428",
+                    Name = "Texto teste 2",
+                    Description = "Description",
+                    Image = "product-2.png",
+                    Price = 488,
+                    Category = "Category 2"
+                },
+                new Product()
+                {
+                    Id = "4357953rtt354g46387f76e8r6347428",
+                    Name = "Texto teste 3",
+                    Description = "Description",
+                    Image = "product-3.png",
+                    Price = 67,
+                    Category = "Category 3"
+                },
+            };
+        }
+    }
+}
